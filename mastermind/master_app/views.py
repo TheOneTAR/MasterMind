d8e971364fceabe388012a0ad85e8c88ad9d06a4
Tiffany Caires <tar3820@gmail.com>
2016-03-12 16:29:45 -0800

    Adding the templates dir and urls for master_app, and setting up landing page.

--- mastermind/master_app/views.py
+++ mastermind/master_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def landing_page(request):
+   '''Landing page VC for the Mastermind app game.'''
+   return render(request, 'master_app/index.html')
