d3819b7a00279d1bf898d97d069d3b1650b1f9b8
Tiffany Caires <tar3820@gmail.com>
2016-03-16 13:49:48 -0700

    Added Code table to admin site
    Registered the Code table to the admin site. Realized I need to reshape database, so TODO has shifted to redoing all of that.

--- mastermind/master_app/admin.py
+++ mastermind/master_app/admin.py
@@ -5,5 +5,6 @@ from .models import *
 # Register your models here.
 admin.site.register(KeyPeg)
 admin.site.register(ColorPeg)
+admin.site.register(Code)
 admin.site.register(Game)
 admin.site.register(Guess)
